9968332da038685ef9853d5a56d1f5c3dbba4015
Paricia Muñoz <patrimdm96@gmail.com>
2020-12-15 20:36:23 +0000

    chat styles, admin home page complete, general web sttyles

--- front/src/components/Chat/Chat.js
+++ front/src/components/Chat/Chat.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react'
+import { Container, Row, Col, Form, Button } from 'react-bootstrap'
 import io from 'socket.io-client'
-import TextField from '@material-ui/core/TextField'
 
-import './Chat.css'
 
 const socket = io("http://localhost:4000", {
   withCredentials: true
@@ -32,42 +31,44 @@ function Chat() {
   const renderChat = () => {
     return chat.map(({ name, message }, index) => (
       <div key={index}>
-        <h3 className="chat-h3">
-          {name}: <span className="chat-span">{message}</span>
-        </h3>
+        <h6 style={{color: "#3e78ac"}}>
+          {name}: <span style={{fontWeight: '300', color: 'black'}}>{message}</span>
+        </h6>
       </div>
     ))
   }
 
   return (
-    <div className="chat-card">
-      <form className="chat-form" onSubmit={onMessageSubmit}>
-        <h1>Messanger</h1>
-        <div className="chat-name-field">
-          <TextField
-            name="name"
-            onChange={e => onTextChange(e)}
-            value={state.name}
-            label="Name"
-          />
-        </div>
-        <div>
-          <TextField
-            name="message"
-            onChange={e => onTextChange(e)}
-            value={state.message}
-            id="outlined-multiline-static"
-            variant="outlined"
-            label="Message"
-          />
-        </div>
-        <button className="chat-button">Send Message</button>
-      </form>
-      <div className="render-chat">
-        {renderChat()}
-      </div>
-    </div>
+    <Container>
+      <h1>Chat Dealz_</h1>
+      <Row style={{display: 'flex', justifyContent: 'space-evenly'}}>
+        <Col md={10} lg={4} style={{margin: '40px 0px'}}>
+          <Form onSubmit={onMessageSubmit}>
+            <Form.Group controlId="name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control name="name" onChange={e => onTextChange(e)} value={state.name} label="Name"  />
+            </Form.Group>
+            <Form.Group controlId="message">
+                <Form.Label>Message</Form.Label>
+                <Form.Control as="textarea" name="message" onChange={e => onTextChange(e)} value={state.message} id="outlined-multiline-static" variant="outlined" label="Message" />
+            </Form.Group>
+            <Button variant="secondary" size="sm" type="submit">Send message</Button>
+          </Form>
+        </Col>
+        <Col md={10} lg={4} style={{ margin: '40px 0px' }}>
+          <section className="form-group">
+            <label className="form-label">Chat</label>
+            <div className="form-control" style={{padding: '15px', height: '200px', overflow: 'scroll'}}>
+              {renderChat()}
+            </div>
+          </section>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
 export default Chat
+
+
+
---
+++ front/src/components/Pages/Admin/Admin-page-card.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react'
+import { Card } from 'react-bootstrap'
+
+import RatingService from '../../../services/rating.service'
+
+const AdminPageCard = ({ name, image, status, id }) => {
+
+    const [state, setState] = useState({ avgRating: undefined })
+    const ratingService = new RatingService()
+
+    useEffect(() => id && getAvgRate())
+
+    const getAvgRate = () => {
+        ratingService
+            .getUserRatings(id)
+            .then(rates => {
+                const avgRate = (rates.data.reduce((acc, elm) => acc + elm.value.valueOf(), 0)) / (rates.data.length)
+                isNaN(parseFloat(avgRate.toFixed(2))) ? setState({ avgRating: 'No rating'}) : setState({ avgRating: `${parseFloat(avgRate.toFixed(2))} / 5`})
+            })
+            .catch(err => console.log('ERROR GETTING AVG RATES', err))
+    }
+
+
+    return (
+        <Card style={{display: 'flex', flexDirection: 'row', marginBottom: '10px', padding: '10px', alignItems: 'center', textAlign: 'left'}}>
+            <img src={image} alt={name} style={{objectFit: 'cover', width: '70px', height: '70px', marginRight: '10px', borderRadius: '3px'}}/>
+            <article style={{width: '100%'}}>
+                <Card.Title style={{ fontSize: '1.2rem', fontWeight: '300'}}>{name}</Card.Title>
+                {status && <Card.Text style={{fontWeight: '300'}}>Status: <span style={status === 'available' ? { color: 'green'} : (status === 'sold' ? { color: 'red'} : { color: 'orange' })}>{status}</span></Card.Text>}
+                {state.avgRating && <Card.Text style={{fontWeight: '300'}}>{state.avgRating}</Card.Text>}
+            </article>
+        </Card>
+    )
+}
+
+
+export default AdminPageCard
