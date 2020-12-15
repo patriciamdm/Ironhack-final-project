05e47cb458fc35b1434cd9abb243d4793c825be5
Paricia Muñoz <patrimdm96@gmail.com>
2020-12-15 17:04:18 +0000

    homepage + create, edit and delete category and location

--- front/src/components/Pages/Admin/Lists/New-element.js
+++ front/src/components/Pages/Admin/Lists/New-element.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { Form, Button } from 'react-bootstrap'
 
+import CategoryService from '../../../../services/category.service'
+import LocationService from '../../../../services/location.service'
+
 
 class NewElm extends Component {
     constructor() {
@@ -8,6 +11,8 @@ class NewElm extends Component {
         this.state = {
             name: ''
         }
+        this.categoryService = new CategoryService()
+        this.locationService = new LocationService()
     }
     
     handleInput = e => this.setState({ [e.target.name]: e.target.value })
@@ -15,13 +20,21 @@ class NewElm extends Component {
     handleSubmit = e => {
         e.preventDefault()
 
-        // this.authService
-        //     .signup(this.state)
-        //     .then(loggedUser => {
-        //         this.props.setUser(loggedUser.data)
-        //         this.props.history.push('/products')
-        //     })
-        //     .catch(err => console.log('ERROR IN SIGN UP', err))
+        this.props.type === 'category' && this.categoryService.newCategory(this.state)
+            .then(() => {
+                this.props.loadList()
+                this.props.hideModal()
+                this.props.handleToast()
+            })
+            .catch(err => console.log('ERROR EDITING CATEGORY', err))
+        
+        this.props.type === 'location' && this.locationService.newLocation(this.state)
+            .then(() => {
+                this.props.loadList()
+                this.props.hideModal()
+                this.props.handleToast()
+            })
+            .catch(err => console.log('ERROR EDITING LOCATION', err))
     }
     
     render() {
--- front/src/components/Shared/PopUps/Toastie.js
+++ front/src/components/Shared/PopUps/Toastie.js
@@ -2,7 +2,7 @@ import { Toast } from 'react-bootstrap'
 
 const Toastie = ({ show, toastText, toastTitle, handleToast, toastType }) => {
     return (
-        <Toast show={show} onClose={() => handleToast(false)} delay={3000} autohide style={{ position: 'fixed', top: 70, right: 10, width: 500 }}>
+        <Toast className="toastie" show={show} onClose={() => handleToast(false)} delay={3000} autohide style={{ position: 'fixed', top: 70, right: 10, width: 450, zIndex: '99999' }}>
             <Toast.Header style={toastType === 'alert' ? { color: 'red', fontWeight: '500'} : { color: 'green', fontWeight: '500'}}>
                 {/* <img src={logo} className="rounded mr-2" alt="" style={{ width: 20, height: 20 }} /> */}
                 <strong className="mr-auto">{toastTitle}</strong>
